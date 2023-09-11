cfa2f39ba1e9baafc58228d06354ac480f1b364e
mk <v.a.stekolschikov@gmail.com>
2023-09-11 17:12:44 +0300

    added redux

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useDispatch, useSelector} from "react-redux";
+import {IStore} from "./store";
+import {increment} from "./store/actions";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const dispatch = useDispatch();
+    const count = useSelector((state: IStore) => state.counter.count);
+    const isEven = useSelector((state: IStore) => state.even.isEven);
+    const joke = useSelector((state: IStore) => state.joke.joke);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <h1>Counter</h1>
+            <p>Count: {count}</p>
+            <p>IsEven: {isEven ? "true" : "false"}</p>
+            <p>joke: {joke}</p>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+        </div>
+    );
 }
 
 export default App
